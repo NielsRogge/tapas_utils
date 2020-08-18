fc23e00f35543625f02059132e44043cd4491e0f
Rogge Niels <niels.rogge@howest.be>
2020-08-18 22:02:42 +0200

    First commit

---
+++ segmented_tensor.py
@@ -0,0 +1,174 @@
+import torch
+
+class IndexMap(object):
+    """Index grouping entries within a tensor."""
+
+    def __init__(self, indices, num_segments, batch_dims=0):
+        """Creates an index.
+        Args:
+            indices: torch.LongTensor of indices, same shape as `values`.
+            num_segments: Scalar tensor, the number of segments. All elements
+            in a batched segmented tensor must have the same number of segments
+            (although many segments can be empty).
+            batch_dims: Python integer, the number of batch dimensions. The first
+            `batch_dims` dimensions of a SegmentedTensor are treated as batch
+            dimensions. Segments in different batch elements are always distinct
+            even if they have the same index.
+        """
+        self.indices = torch.as_tensor(indices)
+        self.num_segments = torch.as_tensor(num_segments)
+        self.batch_dims = batch_dims
+
+    def batch_shape(self):
+        return self.indices.size()[:self.batch_dims] # returns a torch.Size object
+
+
+class ProductIndexMap(IndexMap):
+    """The product of two indices."""
+
+    def __init__(self, outer_index, inner_index):
+        """Combines indices i and j into pairs (i, j).
+        The result is an index where each segment (i, j) is the intersection of
+        segments i and j. For example if the inputs represent table cells indexed by
+        respectively rows and columns the output will be a table indexed by
+        (row, column) pairs, i.e. by cell.
+        The implementation combines indices {0, .., n - 1} and {0, .., m - 1} into
+        {0, .., nm - 1}. The output has `num_segments` equal to
+            `outer_index.num_segments` * `inner_index.num_segments`.
+        Args:
+        outer_index: IndexMap.
+        inner_index: IndexMap, must have the same shape as `outer_index`.
+        """
+        if outer_index.batch_dims != inner_index.batch_dims:
+         raise ValueError('outer_index.batch_dims and inner_index.batch_dims must be the same.')
+
+        super(ProductIndexMap, self).__init__(
+            indices=(inner_index.indices +
+                    outer_index.indices * inner_index.num_segments),
+            num_segments=inner_index.num_segments * outer_index.num_segments,
+            batch_dims=inner_index.batch_dims)
+        self.outer_index = outer_index
+        self.inner_index = inner_index
+
+    def project_outer(self, index):
+        """Projects an index with the same index set onto the outer components."""
+        return IndexMap(
+            indices=(index.indices // self.inner_index.num_segments).type(torch.float).floor().type(torch.long),
+            num_segments=self.outer_index.num_segments,
+            batch_dims=index.batch_dims
+        )
+
+    def project_inner(self, index):
+        """Projects an index with the same index set onto the inner components."""
+        return IndexMap(
+            indices=torch.fmod(index.indices, self.inner_index.num_segments).type(torch.float).floor().type(torch.long),
+            num_segments=self.inner_index.num_segments,
+            batch_dims=index.batch_dims
+        )
+
+def gather(values, index, name='segmented_gather'):
+    """Gathers from `values` using the index map.
+    For each element in the domain of the index map this operation looks up a
+    value for that index in `values`. Two elements from the same segment always
+    get assigned the same value.
+    Args:
+        values: [B1, ..., Bn, num_segments, V1, ...] Tensor with segment values.
+        index: [B1, ..., Bn, I1, ..., Ik] IndexMap.
+        name: Name for the TensorFlow operation.
+    Returns:
+        [B1, ..., Bn, I1, ..., Ik, V1, ...] Tensor with the gathered values.
+    """
+    return torch.gather(
+        input=values, index=index.indices, dim=index.batch_dims)
+
+def flatten(index, name='segmented_flatten'):
+    """Flattens a batched index map (which is typically of shape batch_size, seq_length) to a 1d index map.
+    This operation relabels the segments to keep batch elements distinct. The k-th
+    batch element will have indices shifted by `num_segments` * (k - 1). The
+    result is a tensor with `num_segments` multiplied by the number of elements
+    in the batch.
+    Args:
+        index: IndexMap to flatten.
+        name: Name for the TensorFlow operation.
+    Returns:
+        The flattened IndexMap.
+    """
+    # first, get batch_size as scalar tensor
+    batch_size = torch.prod(torch.tensor(list(index.batch_shape()))) 
+    # next, create offset as 1-D tensor of length batch_size,
+    # and multiply element-wise by num segments (to offset different elements in the batch) e.g. if batch size is 2: [0, 64]
+    offset = torch.arange(start=0, end=batch_size) * index.num_segments  
+    offset = offset.view(index.batch_shape()) 
+    for _ in range(index.batch_dims, len(index.indices.size())): # typically range(1,2)
+        offset = offset.unsqueeze(-1)
+
+    indices = offset + index.indices
+    return IndexMap(
+        indices=indices.view(-1),
+        num_segments=index.num_segments * batch_size,
+        batch_dims=0)
+
+def range_index_map(batch_shape, num_segments, name='range_index_map'):
+    """Constructs an index map equal to range(num_segments).
+    Args:
+        batch_shape: torch.Size object indicating the batch shape 
+        num_segments: int, indicating number of segments
+        name: Name for the TensorFlow operation.
+    Returns:
+        IndexMap of shape batch_shape with elements equal to range(num_segments).
+    """
+    batch_shape = torch.as_tensor(batch_shape) # create a rank 1 tensor vector containing batch_shape (e.g. [2]) 
+    assert len(batch_shape.size()) == 1
+    num_segments = torch.as_tensor(num_segments) # create a rank 0 tensor (scalar) containing num_segments (e.g. 64)
+    assert len(num_segments.size()) == 0
+
+    indices = torch.arange(start=0, end=num_segments) # create a rank 1 vector with num_segments elements 
+    new_tensor = torch.cat([
+        torch.ones_like(batch_shape, dtype=torch.long),
+        num_segments.unsqueeze(dim=0)
+        ],
+        dim=0)
+    # new_tensor is just a vector of [1 64] for example (assuming only 1 batch dimension)
+    new_shape = [int(x) for x in new_tensor.tolist()]
+    indices = indices.view(new_shape) 
+    
+    multiples = torch.cat([batch_shape, torch.as_tensor([1])], dim=0)
+    indices = indices.repeat(multiples.tolist()) 
+    # equivalent (in Numpy:)
+    #indices = torch.as_tensor(np.tile(indices.numpy(), multiples.tolist()))
+    
+    return IndexMap(
+        indices=indices,
+        num_segments=num_segments,
+        batch_dims=list(batch_shape.size())[0])
+
+def _segment_reduce(values, index, segment_reduce_fn, name):
+  """Applies a segment reduction segment-wise."""
+    # Flatten the batch dimensions, as segments ops do not support batching.
+    # However if `values` has extra dimensions to the right keep them
+    # unflattened. Segmented ops support vector-valued operations.
+    flat_index = flatten(index)
+    vector_shape = values.size()[len(index.indices.size()):]) # torch.Size object
+    flattened_shape = torch.cat([torch.as_tensor([-1]), torch.as_tensor(vector_shape)], dim=0)
+    flat_values = torch.view(values, flattened_shape.tolist())
+    segment_means = segment_reduce_fn(
+        data=flat_values,
+        segment_ids=flat_index.indices,
+        num_segments=flat_index.num_segments)
+
+    # Unflatten the values.
+    new_shape = torch.cat(
+        [torch.as_tensor(index.batch_shape()), torch.as_tensor(index.num_segments), torch.as_tensor(vector_shape)], dim=0)
+    output_values = torch.view(segment_means, new_shape.tolist())
+    output_index = range_index_map(index.batch_shape(), index.num_segments)
+    return output_values, output_index
+
+        
+
+
+
+
+
+
+
+    
---
+++ segmented_tensor_test.py
@@ -0,0 +1,141 @@
+import unittest
+import torch
+import numpy as np
+import segmented_tensor
+
+class SegmentedTensorTest(unittest.TestCase):
+    
+    def _prepare_tables(self):
+        """Prepares two tables, both with three distinct rows.
+        The first table has two columns:
+        1.0, 2.0 | 3.0
+        2.0, 0.0 | 1.0
+        1.0, 3.0 | 4.0
+        The second table has three columns:
+        1.0 | 2.0 | 3.0
+        2.0 | 0.0 | 1.0
+        1.0 | 3.0 | 4.0
+        Returns:
+        SegmentedTensors with the tables.
+        """
+        values = torch.tensor([
+            [[1.0, 2.0, 3.0], [2.0, 0.0, 1.0], [1.0, 3.0, 4.0]],
+            [[1.0, 2.0, 3.0], [2.0, 0.0, 1.0], [1.0, 3.0, 4.0]],
+        ])
+        row_index = segmented_tensor.IndexMap(
+            indices=[
+                [[0, 0, 0], [1, 1, 1], [2, 2, 2]],
+                [[0, 0, 0], [1, 1, 1], [2, 2, 2]],
+            ],
+            num_segments=3,
+            batch_dims=1)
+        col_index = segmented_tensor.IndexMap(
+            indices=[
+                [[0, 0, 1], [0, 0, 1], [0, 0, 1]],
+                [[0, 1, 2], [0, 1, 2], [0, 1, 2]],
+            ],
+            num_segments=3,
+            batch_dims=1)
+        return values, row_index, col_index
+
+    def test_product_index(self):
+        _, row_index, col_index = self._prepare_tables()
+        cell_index = segmented_tensor.ProductIndexMap(row_index, col_index)
+        row_index_proj = cell_index.project_outer(cell_index)
+        col_index_proj = cell_index.project_inner(cell_index)
+
+        ind = cell_index.indices
+        self.assertEqual(cell_index.num_segments, 9)
+
+        # Projections should give back the original indices.
+        # we use np.testing.assert_array_equal rather than Tensorflow's assertAllEqual
+        np.testing.assert_array_equal(row_index.indices.numpy(),
+                                row_index_proj.indices.numpy())
+        self.assertEqual(
+            row_index.num_segments,
+            row_index_proj.num_segments)
+        self.assertEqual(row_index.batch_dims, row_index_proj.batch_dims)
+        # We use np.testing.assert_array_equal rather than Tensorflow's assertAllEqual
+        np.testing.assert_array_equal(col_index.indices.numpy(),
+                                col_index_proj.indices.numpy())
+        self.assertEqual(col_index.batch_dims, col_index_proj.batch_dims)
+
+        # The first and second "column" are identified in the first table.
+        for i in range(3):
+            self.assertEqual(ind[0, i, 0], ind[0, i, 1])
+            self.assertNotEqual(ind[0, i, 0], ind[0, i, 2])
+
+        # All rows are distinct in the first table.
+        for i, i_2 in zip(range(3), range(3)):
+            for j, j_2 in zip(range(3), range(3)):
+                if i != i_2 and j != j_2:
+                    self.assertNotEqual(ind[0, i, j], ind[0, i_2, j_2])
+
+        # All cells are distinct in the second table.
+        for i, i_2 in zip(range(3), range(3)):
+            for j, j_2 in zip(range(3), range(3)):
+                if i != i_2 or j != j_2:
+                    self.assertNotEqual(ind[1, i, j], ind[1, i_2, j_2])
+    
+    def test_flatten(self):
+        _, row_index, col_index = self._prepare_tables()
+        row_index_flat = segmented_tensor.flatten(row_index)
+        col_index_flat = segmented_tensor.flatten(col_index)
+
+        shape = [3, 4, 5]
+        batched_index = segmented_tensor.IndexMap(
+            indices=torch.zeros(shape).type(torch.LongTensor),
+            num_segments=1,
+            batch_dims=3)
+        batched_index_flat = segmented_tensor.flatten(batched_index)
+
+        # We use np.testing.assert_array_equal rather than Tensorflow's assertAllEqual
+        np.testing.assert_array_equal(
+            row_index_flat.indices.numpy(),
+            [0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5])
+        np.testing.assert_array_equal(
+            col_index_flat.indices.numpy(),
+            [0, 0, 1, 0, 0, 1, 0, 0, 1, 3, 4, 5, 3, 4, 5, 3, 4, 5])
+        self.assertEqual(
+            batched_index_flat.num_segments.numpy(), np.prod(shape))
+        np.testing.assert_array_equal(
+            batched_index_flat.indices.numpy(), range(np.prod(shape)))
+    
+    def test_range_index_map(self):
+        batch_shape = [3, 4]
+        num_segments = 5
+        index = segmented_tensor.range_index_map(batch_shape, num_segments)
+    
+        self.assertEqual(num_segments, index.num_segments)
+        self.assertEqual(2, index.batch_dims)
+        indices = index.indices
+        # We use np.testing.assert_array_equal rather than Tensorflow's assertAllEqual
+        np.testing.assert_array_equal(list(indices.size()), [3, 4, 5])
+        for i in range(batch_shape[0]):
+            for j in range(batch_shape[1]):
+                # We use np.testing.assert_array_equal rather than Tensorflow's assertAllEqual
+                np.testing.assert_array_equal(indices[i, j, :].numpy(), range(num_segments))
+    
+    """
+    def test_gather(self):
+        values, row_index, col_index = self._prepare_tables()
+        cell_index = segmented_tensor.ProductIndexMap(row_index, col_index)
+
+        # Compute sums and then gather. The result should have the same shape as
+        # the original table and each element should contain the sum the values in
+        # its cell.
+        sums, _ = segmented_tensor.reduce_sum(values, cell_index)
+        cell_sum = segmented_tensor.gather(sums, cell_index)
+        assert cell_sum.size() == values.size()
+
+        # We use np.testing.assert_array_equal rather than Tensorflow's assertAllEqual
+        np.testing.assert_allclose(
+            cell_sum.numpy(),
+            [[[3.0, 3.0, 3.0], [2.0, 2.0, 1.0], [4.0, 4.0, 4.0]],
+            [[1.0, 2.0, 3.0], [2.0, 0.0, 1.0], [1.0, 3.0, 4.0]]]
+        )
+    """
+
+
+if __name__ == '__main__':
+    unittest.main()
